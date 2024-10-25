c788806bdac619bd4bedbb6d569acc1886edc65f
Aran8276 <zahranzn61@gmail.com>
2024-10-25 21:27:27 +0700

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Search } from 'lucide-react'; // Importing the Search icon from Lucide
-import './navbar.scss'; // SCSS for styling Navbar
+import { Search } from "lucide-react"; // Importing the Search icon from Lucide
+import "./Navbar.scss"; // SCSS for styling Navbar
 
 const Navbar = () => {
   return (
@@ -10,12 +9,23 @@ const Navbar = () => {
           Fun Movie
         </a>
         <ul className="menu menu-horizontal">
-          <li><a href="#">Movies</a></li>
-          <li><a href="#">TV</a></li>
-          <li><a href="#">Genres</a></li>
-          <li><a href="#">Favorites</a></li>
+          <li>
+            <a href="#">Movies</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">Genres</a>
+          </li>
+          <li>
+            <a href="#">Favorites</a>
+          </li>
         </ul>
-        <div className="search-icon" onClick={() => window.location.href = "/Search"}>
+        <div
+          className="search-icon"
+          onClick={() => (window.location.href = "/Search")}
+        >
           <Search size={24} /> {/* You can adjust the size as needed */}
         </div>
       </div>
